a6cee1f3e429b138b0c1c6d855de5c940e459672
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-08-18 15:46:07 +0800

    ACTIVITY 9

---
+++ activity9.py
@@ -0,0 +1,13 @@
+a = 2
+b = 5
+
+c = 9
+d = 21 
+
+username = 'iyaaaa'
+password = 'iyalovesroi'
+
+print( (username == 'iyaaaa') and (password == 'iyalovesroi') )
+
+#print( (a < b) and (c < d) )
+#print( (a > b) or (c < d) )
