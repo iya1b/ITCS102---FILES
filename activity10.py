9df16a0a3d0d3930ad739e454c7779f58a0c9010
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-08-27 15:07:48 +0800

    ACTIVITY 10

---
+++ activity10.py
@@ -0,0 +1,8 @@
+username = "iyabanana"
+password = "iluvpink"
+
+uname = input("Enter username --> ")
+pword = input("Enter password --> ")
+
+if (uname == username) and (pword == password): print("ACCESS GRANTED")
+else: print("ACCESS DENIED")
