06a6e635f1f1399d512f787171614117b51824b5
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-09-26 22:53:01 +0800

    code_challenge7.py

---
+++ code_challenge7.py
@@ -0,0 +1,8 @@
+print("COUNTDOWN TIME SIMULATOR")
+cd = eval(input("Please enter the starting number for countdown --> "))
+
+print("The countdown started: ")
+for time in range (cd, 0, -1):
+    print(time)
+   
+print("LIFTOFF!")
