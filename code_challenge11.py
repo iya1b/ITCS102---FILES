4835c1000ec067e38aa8afe273fa8e3097b18c37
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-10-25 21:03:53 +0800

    CODE CHALLENGE 11

---
+++ code_challenge11.py
@@ -0,0 +1,10 @@
+print("\t\t *", end=" ")
+
+for iya in range (1, 11, 1):
+    for iya1 in range (10, iya, -1):
+        print(" ", end=" ")
+    for iya2 in range (1, iya, 1):
+        print("*", end=" ")
+    for iya3 in range (1, iya, 1):
+        print("*", end=" ")
+    print()
