2f80ae1d59160e3e22ba8c7f3ca36fffc1a6bfb7
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-10-25 21:07:50 +0800

    ACTIVITY 20

---
+++ activity20.py
@@ -0,0 +1,17 @@
+for iya in range (1, 11, 1):
+    for iya1 in range (11, iya, -1):
+        print(" ", end=" ")
+    for iya2 in range (1, iya, 1):
+        print("*", end=" ")
+    for iya3 in range (1, iya, 1):
+        print("*", end=" ")
+    print()
+
+for iyaa in range (1, 11, 1):
+    for iyaa1 in range (1, iyaa, 1):
+        print(" ", end=" ")
+    for iyaa2 in range (11, iyaa, -1):
+        print("*", end=" ")
+    for iyaa3 in range (10, iyaa, -1):
+        print("*", end=" ")
+    print()
