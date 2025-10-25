a73a09bea6a5dbb77e5321849b0a1b383c339007
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-10-25 21:03:01 +0800

    CODE CHALLENGE 10

--- code_challenge10.py
+++ code_challenge10.py
@@ -1,4 +1,4 @@
-for iya in range(10, 0, -1):
-    for iya2 in range(1, iya, 1):
-        print("*" ,end=" ")
+for iya in range (10, 0, -1):
+    for iya1 in range (1, iya, 1):
+        print("*", end=" ")
     print()
