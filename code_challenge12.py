b1bea17fc02a56011a870ff3b662b999e2b35dec
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-10-25 21:04:43 +0800

    CODE CHALLENGE 12

---
+++ code_challenge12.py
@@ -0,0 +1,8 @@
+for iya in range (1, 7, 1):
+    for iya1 in range (7, iya, -1):
+        print(" ", end=" ")
+    for iya2 in range (iya, 0, -1):
+        print(iya2, end=" ")
+    for iya3 in range (2, iya + 1, 1):
+        print(iya3, end=" ")
+    print()
